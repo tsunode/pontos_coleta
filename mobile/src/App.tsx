63129b8a5997b49d24585c18e8b830dc5c7d94b0
tsunoda1 <tsunoda@b2ml.com.br>
2020-09-19 13:55:16 -0300

    feat: create mobile structure

---
+++ mobile/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const src: React.FC = () => {
+  return (
+    <View>
+      <Text>Teste</Text>
+    </View>
+  );
+};
+
+export default src;
